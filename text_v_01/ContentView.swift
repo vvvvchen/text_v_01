1b4e4903d2876fa92b9e58171ce2f6ca7b1b05e5
Valerie Chen <vchenin620@gmail.com>
2023-08-08 12:23:58 +0800

    Initial Commit

---
+++ text_v_01/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  text_v_01
+//
+//  Created by Mac on 2023/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ text_v_01/text_v_01App.swift
@@ -0,0 +1,17 @@
+//
+//  text_v_01App.swift
+//  text_v_01
+//
+//  Created by Mac on 2023/8/8.
+//
+
+import SwiftUI
+
+@main
+struct text_v_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
